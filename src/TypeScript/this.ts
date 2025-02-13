b9bed5c204e3efc9c6ae4aefbc95e5e70ed1b2b3
fsande <fsande@ull.es>
2025-02-13 11:53:54 +0000

    Function documentation

--- src/TypeScript/this.ts
+++ src/TypeScript/this.ts
@@ -6,8 +6,14 @@
   *
   * @author F. de Sande
   * @since Feb 26, 2023
-  * @desc TS classes. 
-  *       this
+  * @desc TS classes. this Keyword
+  *
+  *       En este programa, la función main se está exportando (export) lo cual posibilitaría su utilización
+  *       en otro módulo, mediante importación. Se hace solo a efectos ilustrativos.
+  *       main() se ha escrito como una función flecha asíncrona. Una función asíncrona podría contener 
+  *       operaciones que se ejecutan de manera no bloqueante, como llamadas tareas que toman tiempo. 
+  *       Si no se quiere que la función sea asíncrona, basta eliminar la palabra 'async' de la definición de
+  *       la función. En este caso concreto, todo funcionaría del mismo modo.
   */
 
 class Leaf {
