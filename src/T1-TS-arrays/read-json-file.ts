4abad43edd629978180e501ae85552927f7199de
fsande <fsande@ull.es>
2025-03-16 18:11:43 +0000

    Comments fixed

--- src/T1-TS-arrays/read-json-file.ts
+++ src/T1-TS-arrays/read-json-file.ts
@@ -8,13 +8,14 @@
  * @author F. de Sande
  * @since Mar 19, 2023
  * @description Read a JSON object from file
+ *              Play play with the program by commenting out and uncommenting the lines in the main() function
  */
 
 import * as data from './country-by-abbreviation.json';
 
 const main = function (): void {
-  console.log(typeof(data));  
-  console.log(data);  
+  // console.log(typeof(data));  
+  // console.log(data);  
   for (const datum of data) {
     console.log(datum.country, datum.abbreviation);
   }
