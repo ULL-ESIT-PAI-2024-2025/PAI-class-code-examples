19e24cd6a90082b66b0949fd4e94480682edf841
fsande <fsande@ull.es>
2025-03-16 14:09:55 +0000

    Comments fixed

--- src/T1-TS-arrays/slice-example.ts
+++ src/T1-TS-arrays/slice-example.ts
@@ -7,6 +7,9 @@
  * @author F. de Sande
  * @since Mar 2, 2023
  * @desc slice example
+ *       The slice() method of Array instances returns a shallow copy of a portion of an array into a new array 
+ *       object selected from start to end (end not included) where start and end represent the index of items in that array. 
+ *       The original array will not be modified.
  * @see @link{https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice}
  */
 
